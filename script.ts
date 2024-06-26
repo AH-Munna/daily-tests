449f4d96996b7e3bdf24ead6ef35d1f989e3b9f5
Ahsanul Haque Munna <96417158+AH-Munna@users.noreply.github.com>
2024-06-26 10:28:34 +0600

    Update and rename script.js to script.ts

---
+++ script.ts
@@ -0,0 +1,19 @@
+console.log("hello");
+const a = 20;
+const userAuthed = true;
+if (userAuthed) {
+  console.log("welcome");
+}
+// const btn = document.getElementById("bid");
+// btn.innerHTML = "Click Me";
+// console.log(btn);
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+    const joke = res.value;
+    console.log(joke);
+  });
